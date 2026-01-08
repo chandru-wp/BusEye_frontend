58d792076dc50f2030a42fe6ce7d2389bb79c5f1
Chandru <ck6582293@gmail.com>
2026-01-08 07:59:24 +0530

    Updates

--- registerSW.js
+++ registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/BusEye_frontend/sw.js', { scope: '/BusEye_frontend/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('./sw.js', { scope: './' })})}
